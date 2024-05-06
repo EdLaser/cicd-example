2dc889b6da8dfbd6a82d6a4cacc0f73458c49a4d
= <=>
2024-05-06 10:03:25 +0200

    try to fix

--- todo-api-python/app/1todo_test.py
+++ todo-api-python/app/1todo_test.py
@@ -1,12 +1,21 @@
 import pytest
-from sqlalchemy.orm import Session
+from os import environ
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from .sql_app.models import ToDo
+from .sql_app.models import Base
+
 
 @pytest.fixture
 def session():
-    db = Session()
+    engine = create_engine(environ.get("DATABASE_URL"))
+    Base.metadata.create_all(engine)
+    session_local = sessionmaker(
+        autocommit=False, autoflush=False, bind=engine)
+    db = session_local()
     yield db
     db.close()
+
 
 def test_create_todo(session):
     todo = ToDo(title="Test Todo", description="This is a test todo.")
@@ -17,6 +26,7 @@ def test_create_todo(session):
     assert todo.description == "This is a test todo."
     assert not todo.is_done
 
+
 def test_update_todo(session):
     todo = ToDo(title="Test Todo", description="This is a test todo.")
     session.add(todo)
@@ -24,6 +34,7 @@ def test_update_todo(session):
     todo.title = "Updated Todo"
     session.commit()
     assert todo.title == "Updated Todo"
+
 
 def test_delete_todo(session):
     todo = ToDo(title="Test Todo", description="This is a test todo.")
