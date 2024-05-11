227e272f93975ac023aec662a1224ccf01e5c01a
Ruben Kraus <s80350@htw-dresden.de>
2024-05-11 16:33:20 +0200

    udpate

--- todo-api-python/app/sql_app/database.py
+++ todo-api-python/app/sql_app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://mysecretuser:mysecretpassword@db/todos"
+SQLALCHEMY_DATABASE_URL = "postgresql://mysecretuser:mysecretpassword@localhost/todos"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
